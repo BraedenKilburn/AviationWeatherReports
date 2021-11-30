199e58b2a94bbc60efeec85cfcd24d2fccfb5f64
Braeden Kilburn <braedenk99@gmail.com>
2021-11-30 10:58:00 -0700

    Server can call and save reports

--- back-end/server.js
+++ back-end/server.js
@@ -12,23 +12,37 @@ const app = express();
 let connectionString = `mongodb://localhost:27017/weather`
 
 let db;
+let airports;
+let metars;
+let tafs;
 
 // Connect to our MongoDB
 MongoClient.connect(
     connectionString,
     { useNewUrlParser: true, useUnifiedTopology: true },
-    function (err, client) {
+    async function (err, client) {
         if (err) 
             console.log(err);
         else
         {
             db = client.db();
+			airports = db.collection('airport');
+			metars = db.collection('metar');
+			tafs = db.collection('taf');
+
+			// If we've specified our indexes previously, drop them so we can reset them
+			if ((await airports.indexes()).length > 1)
+				airports.dropIndex( "accessedOn_1" );
+			if ((await metars.indexes()).length > 1)
+				metars.dropIndex( "accessedOn_1" );
+			if ((await tafs.indexes()).length > 1)
+				tafs.dropIndex( "accessedOn_1" );
 
             // Use Mongo's "Time to Live" collection feature to remove elements that have been there for a certain
             // length of time (i.e., how many seconds have we stored this for?)
-            db.collection('airport').createIndex({"retrievedOn": 1}, {expireAfterSeconds: 86400}); // Expires after 24 hours
-            db.collection('metar').createIndex({"retrievedOn": 1}, {expireAfterSeconds: 900}); // Expires after 15 minutes
-            db.collection('taf').createIndex({"retrievedOn": 1}, {expireAfterSeconds: 900}); // Expires after 15 minutes
+            airports.createIndex({"accessedOn": 1}, {expireAfterSeconds: 86400}); // Expires after 24 hours
+            metars.createIndex({"accessedOn": 1}, {expireAfterSeconds: 900}); // Expires after 15 minutes
+            tafs.createIndex({"accessedOn": 1}, {expireAfterSeconds: 900}); // Expires after 15 minutes
         }
     }
 );
@@ -73,7 +87,7 @@ const getMETAR = async (icao) => {
 		const json = res.data;
 
 		// Throw error for an empty object
-		if (json.results === 0) throw "Error in GetMETAR";
+		if (json.results === 0) throw "No available METAR";
 
 		return json.data[0];
 	} catch (e) {
@@ -104,29 +118,129 @@ const getTAF = async (icao) => {
 	}
 };
 
-// Get airport information (hard-coded KVGT for now)
-app.get('/airport', (req, res) => {
-    // db.collection('airport').insertOne({text: req.body.text}
+// Get airport information
+app.get('/airport/:icao', async (req, res) => {
+	try {
+		let icao = req.params.icao.toUpperCase();
+
+		// Check if the db has the airport
+		const airport = await airports.findOne({icao: icao});
+		
+		// If the airport was found in our DB
+		if (airport)
+		{
+			console.log(`${icao} airport information found!`);
+			res.send(airport);
+		}
+		// The airport was not found in our DB
+		else
+		{
+			console.log(`\n${icao} airport information not found in DB\nAttemptin to fetch airport info and add it to our DB`);
+
+			// Try and get the airport (possibly not valid)
+			try {
+				const airport = await getAirport(icao);
+
+				airport.accessedOn = new Date();
+	
+				airports.insertOne(airport);
+				console.log(`\n${icao}'s airport information was successfully added to the DB`);
+				res.send(airport);	
+			}
+			catch (error) {
+				console.log("\n", error);
+				res.sendStatus(404);
+			}
+		}
+	}
+	catch (error) {
+		console.log(error);
+		res.sendStatus(500);
+	}
 });
 
-// Add airport information
-app.post('/airport', async (req, res) => {
-    const airport = await getAirport("KVGT");
-    airport.retrievedOn = new Date();
+// Get METAR report
+app.get('/metar/:icao', async (req, res) => {
+	try {
+		let icao = req.params.icao.toUpperCase();
 
-    db.collection('airport').insertOne(airport);
+		// Check if the db has the airport's METAR report
+		const metar = await metars.findOne({icao: icao});
+		
+		// If the METAR was found in our DB
+		if (metar)
+		{
+			console.log(`${icao} METAR information found!`);
+			res.send(metar);
+		}
+		// The METAR was not found in our DB
+		else
+		{
+			console.log(`\n${icao} METAR information not found in DB\nAttemptin to fetch METAR and add it to our DB`);
 
-    res.send(airport);
+			// Try and get the METAR (possibly not valid)
+			try {
+				const metar = await getMETAR(icao);
+
+				metar.accessedOn = new Date();
+	
+				metars.insertOne(metar);
+				console.log(`\n${icao}'s METAR report was successfully added to the DB`);
+				res.send(metar);	
+			}
+			catch (error) {
+				console.log("\n", error);
+				res.sendStatus(404);
+			}
+		}
+	}
+	catch (error) {
+		console.log(error);
+		res.sendStatus(500);
+	}
 });
 
-app.put('/', (req, res) => {
-    res.send('I am updated.\n');
-});
+// Get TAF report
+app.get('/taf/:icao', async (req, res) => {
+	try {
+		let icao = req.params.icao.toUpperCase();
 
-app.delete('/', (req, res) => {
-    res.send('All my memories have been deleted. Are you happy now?\n');
+		// Check if the db has the airport's TAF report
+		const taf = await tafs.findOne({icao: icao});
+		
+		// If the TAF was found in our DB
+		if (taf)
+		{
+			console.log(`${icao} TAF information found!`);
+			res.send(taf);
+		}
+		// The TAF was not found in our DB
+		else
+		{
+			console.log(`\n${icao} TAF information not found in DB\nAttemptin to fetch TAF and add it to our DB`);
+
+			// Try and get the TAF (possibly not valid)
+			try {
+				const taf = await getTAF(icao);
+
+				taf.accessedOn = new Date();
+	
+				tafs.insertOne(taf);
+				console.log(`\n${icao}'s TAF report was successfully added to the DB`);
+				res.send(taf);	
+			}
+			catch (error) {
+				console.log("\n", error);
+				res.sendStatus(404);
+			}
+		}
+	}
+	catch (error) {
+		console.log(error);
+		res.sendStatus(500);
+	}
 });
 
 app.listen(PORT, () => {
-    console.log(`Node.js App running on port ${PORT}`);
+    console.log(`Weather API running on port ${PORT}`);
 });
